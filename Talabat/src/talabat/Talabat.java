e78a28284b0fe28416cf875d5c19974ff696a2db
mariammahmoud18 <mariammahmoud18@users.noreply.github.com>
2020-12-24 17:00:41 +0200

    Update Talabat.java

--- Talabat/src/talabat/Talabat.java
+++ Talabat/src/talabat/Talabat.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package talabat;
-
+import java.util.Scanner;
 /**
  *
  * @author pc
@@ -16,6 +16,7 @@ public class Talabat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input= new Scanner(System.in);
     }
     
 }
